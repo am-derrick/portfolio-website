aa47007ee5f2ac3045829ed7d7586909715ce460
Derrick Ampire <ampire90@gmail.com>
2022-04-18 14:42:41 +0300

    First files added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src={images.logo} alt='logo' />
+        </div>
+        <ul>
+            {['home', 'about', 'work', 'skills', 'contact']}
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
